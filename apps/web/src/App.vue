1a35ab4e02948809f3679178d01b3ed89a815ecd
jxianc <jingxianchai01@gmail.com>
2022-05-24 00:15:25 -0500

    chore: setup tailwindcss, color theme, base page

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,71 +1,64 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+export type UserTheme = 'light' | 'dark'
+
+const setTheme = (theme: UserTheme) => {
+  localStorage.setItem('user-theme', theme)
+  userTheme.value = theme
+  document.documentElement.className = theme
+}
+
+const getTheme = (): UserTheme => {
+  return localStorage.getItem('user-theme') as UserTheme
+}
+
+// const toggleTheme = (): void => {
+//   const activeTheme = localStorage.getItem('user-theme')
+//   if (activeTheme === 'light') {
+//     setTheme('dark')
+//   } else {
+//     setTheme('light')
+//   }
+// }
+
+const getMediaPreference = (): UserTheme => {
+  const hasDarkPreference = window.matchMedia(
+    '(prefers-color-scheme: dark)',
+  ).matches
+  if (hasDarkPreference) {
+    return 'dark'
+  } else {
+    return 'light'
+  }
+}
+
+const userTheme = ref<UserTheme>(getTheme() || getMediaPreference())
+
+onMounted(() => setTheme(userTheme.value))
+</script>
 
 <template>
-  <header></header>
-  <main>
-    <div>bupu</div>
+  <main
+    class="flex flex-col space-y-4 min-h-screen max-h-screen text-center items-center justify-center"
+  >
+    <!-- <header
+      class="absolute px-4 top-2 w-full font-semibold flex justify-between"
+    >
+      <div>light / dark</div>
+      <div>source</div>
+    </header> -->
+    <div class="text-3xl font-bold">
+      <h1>ブープ</h1>
+      <h2>Bū pu</h2>
+    </div>
+    <p class="text-4xl font-bold">69420 bp</p>
+    <footer class="absolute px-4 bottom-2 w-full font-semibold">
+      27348 people booping
+    </footer>
   </main>
 </template>
 
 <style>
 @import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
