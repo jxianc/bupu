d910420e3eff24de7eb23a4c95b08aead08cfe2a
jxianc <jingxianchai01@gmail.com>
2022-05-26 21:20:11 -0500

    chore: added source link

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -9,16 +9,20 @@ const toggleTheme = useToggle(isDark)
   <main
     class="flex flex-col space-y-4 min-h-screen max-h-screen text-center items-center justify-center"
   >
-    <header
-      class="absolute px-4 top-4 w-full font-semibold flex justify-between"
-    >
+    <header class="absolute px-4 top-4 w-full flex justify-between">
       <button
         class="bg-gray-200 dark:bg-zinc-800 px-2 py-1 rounded-[0.3em] font-semibold dark:hover:bg-zinc-700 hover:bg-zinc-300"
         @click="toggleTheme()"
       >
         {{ isDark ? 'Light' : 'Dark' }}
       </button>
-      <div>source</div>
+      <a
+        href="https://github.com/jxianc/bupu"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="font-semibold hover:underline hover:text-gray-500"
+        >source</a
+      >
     </header>
     <div class="text-3xl font-bold">
       <h1>ブープ</h1>
